6c64ff2c7f540432ef0ec80b9b68ff08a46f984c
Glitch (lab01-webapp-2023) <none>
2023-01-11 13:33:46 +0000

    🌗🔆 Updated with Glitch

--- script.js
+++ script.js
@@ -37,9 +37,3 @@ message.addEventListener("click", () => {
   message.style.display = "none";
 });
 
-const buttons = document.querySelectorAll('button');
-console.log(buttons);
-
-buttons[2].addEventListener("mouseout", () => {
-  console.log("Moved away from button 3");
-});
